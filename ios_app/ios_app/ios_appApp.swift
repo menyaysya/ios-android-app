9e56db71e61947df5bedcad70bd5902e4bde2a71
vixxer <83184947+menyaysya@users.noreply.github.com>
2023-10-18 10:45:06 +0300

    create project

---
+++ ios_app/ios_app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios_app
+//
+//  Created by Денис Гончарук on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
